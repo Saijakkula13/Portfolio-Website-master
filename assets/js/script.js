b398975b99b91e6932b96245664a7d75dc297285
SAI KUMAR JAKKULA <saikumarjakkula23@gmail.com>
2025-09-09 14:24:46 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Software Engineer", "Python Developer", "Data Analytics", "IoT Development", "Frontend Developer"],
+    strings: ["Software Engineer", "Python Developer", "AI Engineer", "Data Analytics", "IoT", "Frontend Developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
